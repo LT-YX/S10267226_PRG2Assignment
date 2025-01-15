0c366b0f78795dcdc346c27441e5d74998bf0009
Lovette <106056572+LT-YX@users.noreply.github.com>
2025-01-15 17:13:03 +0800

    Added names and student id

--- PRG02_Assignment/PRG02_Assignment/Program.cs
+++ PRG02_Assignment/PRG02_Assignment/Program.cs
@@ -1,1 +1,6 @@
-﻿// 
+﻿//==========================================================
+// Student Number of Student 1 : S10267226
+// Student 1 Name : Lovette Tew Yu Xin
+// Student Number of Student 2 : S
+// Student 2 Name : 
+//==========================================================
