7120fdd34b7108a62227f7a2ad6a342994b22583
ZzYouAll <zandertansz@gmail.com>
2025-01-15 17:55:57 +0800

    ZT-Test2

---
+++ PRG02_Assignment/PRG02_Assignment/test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG02_Assignment
+{
+    internal class test
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
