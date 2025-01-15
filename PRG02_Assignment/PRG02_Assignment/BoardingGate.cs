f49109e81ad28c12a78a56ccc806d4425982a975
ZzYouAll <zandertansz@gmail.com>
2025-01-15 18:09:25 +0800

    Added Zander's Student Info

---
+++ PRG02_Assignment/PRG02_Assignment/BoardingGate.cs
@@ -0,0 +1,6 @@
+//==========================================================
+// Student Number of Student 1 : S10267226
+// Student 1 Name : Lovette Tew Yu Xin
+// Student Number of Student 2 : S10265760
+// Student 2 Name : Tan Sheng Zhe Zander
+//==========================================================
