3abcce7027942d5bbec0ac08a2fe382f0a7ddd70
Lovette <106056572+LT-YX@users.noreply.github.com>
2025-01-15 18:26:01 +0800

    Added Classes

--- PRG02_Assignment/PRG02_Assignment/LWTTFlight.cs
+++ PRG02_Assignment/PRG02_Assignment/LWTTFlight.cs
@@ -1,6 +1,21 @@
+<<<<<<< Updated upstream
 //==========================================================
 // Student Number of Student 1 : S10267226
 // Student 1 Name : Lovette Tew Yu Xin
 // Student Number of Student 2 : S10265760
 // Student 2 Name : Tan Sheng Zhe Zander
 //==========================================================
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG02_Assignment
+{
+    public class LWTTFlight : Flight
+    {
+    }
+}
+>>>>>>> Stashed changes
--- PRG02_Assignment/PRG02_Assignment/Program.cs
+++ PRG02_Assignment/PRG02_Assignment/Program.cs
@@ -1,6 +1,14 @@
 ﻿//==========================================================
 // Student Number of Student 1 : S10267226
 // Student 1 Name : Lovette Tew Yu Xin
+<<<<<<< Updated upstream
 // Student Number of Student 2 : S10265760
 // Student 2 Name : Tan Sheng Zhe Zander
 //==========================================================
+=======
+// Student Number of Student 2 : S
+// Student 2 Name : 
+//==========================================================
+
+using PRG02_Assignment;
+>>>>>>> Stashed changes
