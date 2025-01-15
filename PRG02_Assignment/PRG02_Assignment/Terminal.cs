25a9a3aacb00f73dd40becd4ca17b6e0f6042fd7
Lovette <106056572+LT-YX@users.noreply.github.com>
2025-01-15 18:46:19 +0800

    edited comments

--- PRG02_Assignment/PRG02_Assignment/Terminal.cs
+++ PRG02_Assignment/PRG02_Assignment/Terminal.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿//==========================================================
+// Student Number of Student 1 : S10267226
+// Student 1 Name : Lovette Tew Yu Xin
+// Student Number of Student 2 : S10265760
+// Student 2 Name : Tan Sheng Zhe Zander
+//==========================================================
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
