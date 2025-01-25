00469a1534c8a352edd36eb41a070038d62e7b50
ZzYouAll <zandertansz@gmail.com>
2025-01-25 17:42:44 +0800

    Completion of task 1 for submission

--- S10267226_PRG2Assignment/S10267226_PRG2Assignment/Program.cs
+++ S10267226_PRG2Assignment/S10267226_PRG2Assignment/Program.cs
@@ -9,7 +9,8 @@ using S10267226_PRG2Assignment;
 
 //
 Dictionary<string, Flight> flightDictionary = new Dictionary<string, Flight>();
-
+Dictionary<string, Airline> airlineDictionary = new Dictionary<string, Airline>();
+Dictionary<string, BoardingGate> boardingGateDictionary = new Dictionary<string, BoardingGate>();
 
 // Main Program
 LoadFlights();
@@ -32,6 +33,51 @@ void DisplayMenu()
         "\n6. Modify Flight Details" +
         "\n7. Display Flight Schedule" +
         "\n0. Exit\n");
+}
+
+// Feature 1
+
+void ReadAirlines()
+{
+    try
+    {
+        using (StreamReader sr = new StreamReader("airlines.csv"))
+        {
+            string s = sr.ReadLine(); // Reads header and ignores it
+            while ((s = sr.ReadLine()) != null)
+            {
+                string[] information = s.Split(",");
+                Airline a = new Airline(information[0], information[1]);
+                airlineDictionary.Add(information[1], a);
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error reading airlines file: {ex.Message}");
+    }
+}
+
+void ReadBoardingGates()
+{
+    try
+    {
+        using (StreamReader sr = new StreamReader("boardinggates.csv"))
+        {
+            string s = sr.ReadLine(); // Reads header and ignores it
+            while ((s = sr.ReadLine()) != null)
+            {
+                string[] information = s.Split(",");
+                BoardingGate bg = new BoardingGate(information[0], Convert.ToBoolean(information[1]), Convert.ToBoolean(information[2]), Convert.ToBoolean(information[3]));
+                boardingGateDictionary.Add(information[0], bg);
+            }
+        }
+    }
+
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error reading boarding gates file: {ex.Message}");
+    }
 }
 
 // Feature 2
